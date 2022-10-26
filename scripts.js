d7fae8c6a3a8fc14c825a5e4c3a4a1c463059c3d
Sebastian Bernachia <sebastian.bernachia@outlook.com>
2022-10-26 18:45:55 -0300

    Clase 04 terminada

--- scripts.js
+++ scripts.js
@@ -1,23 +1,44 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {
+    const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    input.value = "";
+    const createTask = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
+        input.value = "";
 
-    const list = document.querySelector("[data-list]")
-    const task = document.createElement("li");
-    task.classList.add("card");
-    const content = `
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+        const list = document.querySelector("[data-list]")
+        const task = document.createElement("li");
+        task.classList.add("card");
 
-    list.appendChild(task);
-}
+        const taskContent = document.createElement("div");
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(titleTask);
+        
+        task.appendChild(taskContent);
+        const content = `
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+        
 
-btn.addEventListener("click", createTask);
+        list.appendChild(task);
+        
+    }
+    btn.addEventListener("click", createTask);
+    
+    const completeTask = (event) => {
+        const element = event.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon")
+        element.classList.toggle("far");
+    }
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        return i
+    }
+})();
